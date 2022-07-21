a46765836c33fde4ca2c2fb9163a75d833de6784
Davit Injgia <dato.injgia18@gmail.com>
2022-07-21 15:56:55 +0400

    Initial Commit

---
+++ homeWork19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork19
+//
+//  Created by Dato on 21.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
